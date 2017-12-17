a6b417d735a5226366bfcfd67d0a53b3d56efe44
rakesh <raku1109@gmail.com>
2017-12-17 12:42:00 +0530

    Updated the UserDbOperation with CRUD

--- TelephoneDirectory.SqlRespository/UserDbOperations.cs
+++ TelephoneDirectory.SqlRespository/UserDbOperations.cs
@@ -1,4 +1,6 @@
-﻿using System.Data.SqlClient;
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
 using Dapper;
 using TelephoneDirectory.Entities;
 
@@ -19,6 +21,54 @@ namespace TelephoneDirectory.SqlRespository
                     name = user.Name,
                     address = user.Address
                 });
+            }
+        }
+
+        public void Update(User user, int id )
+        {
+            const string query = "UPDATE Users SET Name = @name and Address = @address where Id = @Id";
+
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                conn.Execute(query, new
+                {
+                    name = user.Name,
+                    address = user.Address
+                });
+            }
+
+        }
+
+        public void Delete(int uid)
+        {
+            const string query = "Delete from Users where Id = @Id";
+
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                conn.Execute(query, new
+                {
+                    Id = uid
+                });
+
+            }
+        }
+
+        public List<User> GetAll()
+        {
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                return conn.Query<User>("SELECT * FROM Users").ToList();
+            }
+        }
+
+        public User GetById(int uid)
+        {
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                return conn.Query<User>("SELECT * FROM Users WHERE Id=@Id", new
+                {
+                   Id = uid
+                }).FirstOrDefault();
             }
         }
     }
