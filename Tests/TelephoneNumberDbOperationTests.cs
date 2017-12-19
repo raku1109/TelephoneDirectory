cd3fa0384db2e6ac02dd1568e99bd47ebbbbe485
rakesh <raku1109@gmail.com>
2017-12-19 14:29:15 +0530

    Unit Testing for TelephoneNumber_Create with Invalid Inputs done.

--- Tests/TelephoneNumberDbOperationTests.cs
+++ Tests/TelephoneNumberDbOperationTests.cs
@@ -28,5 +28,18 @@ namespace Tests
 
             }
         }
+
+        [TestCase(null,null,null)]
+        [TestCase(1,null,null)]
+        [TestCase(null,"+91 9820275604",null)]
+        [TestCase(null,null,"Work")]
+
+        public void When_Create_Is_Called_With_Invalid_Data_It_Should_Throw_A_SqlException(int uid, string number, string type)
+        {
+            var telephoneDbOperation = new TelephoneNumberDbOperations();
+            var telephone = new TelephoneNumber() {UId= uid,PhoneNumber = number,NumberType = type};
+            Assert.Throws<SqlException>(() => telephoneDbOperation.Create(telephone));
+
+        }
     }
 }
