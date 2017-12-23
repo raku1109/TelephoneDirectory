70aeac884f2dbf22cb860c41660f0606a5413704
rakesh <raku1109@gmail.com>
2017-12-23 11:52:36 +0530

    Created a TelephoneNumberServices Class

---
+++ UserServices/TelephoneNumberServices.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using TelephoneDirectory.SqlRespository;
+using TelephoneDirectory.Entities;
+
+namespace UserServices
+{
+    class TelephoneNumberServices
+    {
+        public bool Create(TelephoneNumber telephone)
+        {
+            var numberServices = new TelephoneNumberDbOperations();
+            numberServices.Create(telephone);
+            return false;
+        }
+
+        public void Update(TelephoneNumber telephone)
+        {
+            var numberServices = new TelephoneNumberDbOperations();
+            numberServices.Update(telephone);
+        }
+
+        public void Delete(TelephoneNumber telephone)
+        {
+            var numberServices = new TelephoneNumberDbOperations();
+            numberServices.Delete(telephone);
+        }
+
+        public TelephoneNumber GetById(TelephoneNumber telephone)
+        {
+            var numberServices = new TelephoneNumberDbOperations();
+            numberServices.GetById(telephone);
+            return telephone;
+        }
+
+        public List<TelephoneNumber> GetAll()
+        {
+            var numberServices = new TelephoneNumberDbOperations();
+            List<TelephoneNumber> number = numberServices.GetAll();
+            return number;
+        }
+    }
+}
