06cd44b704bc2d78c8491755f5a02978b89a50d2
rakesh <raku1109@gmail.com>
2017-12-23 12:57:26 +0530

    Unit Testing for Create method of UserServices without Validation done.

---
+++ Tests/UserServiesTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TelephoneDirectory.SqlRespository;
+using TelephoneDirectory.Entities;
+
+
+namespace Tests
+{
+    [TestFixture]
+    class UserServiesTest
+    {
+        [Test]
+        public void Create_User_Via_Service()
+        {
+            var userServices = new UserServices.UserServices();
+            var user = new User() {Name = "Rakesh",Address = "Mumbai"};
+            userServices.Create(user);
+
+            
+
+            Assert.IsTrue(user.Id >0);
+            Assert.Pass();
+
+        }
+    }
+}
